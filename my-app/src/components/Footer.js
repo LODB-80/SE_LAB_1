f44f3aa79a42cdedf7ccb20f04f3fba5b45c94a1
amirdr81 <amirmohammadd@yahoo.com>
2025-03-17 19:57:28 +0330

    add Footer.js to src/components

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Contact us: amoohashem@gmail.com | 123-456-7890</p>
+    </footer>
+  );
+};
+
+export default Footer;
