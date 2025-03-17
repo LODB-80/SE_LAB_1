7a5926d7aa91a32a946f96ad1944f8733a6bf295
kian <kianizadpanah@gmail.com>
2025-03-17 20:32:28 +0330

    fix the bug and imports inside index.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Header from "./src/components/Header";
-import Body from "./src/components/Body";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
