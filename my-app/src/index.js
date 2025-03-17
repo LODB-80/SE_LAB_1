f6429fa79197434c77659e0945f0181aa86f1ad1
kian <kianizadpanah@gmail.com>
2025-03-17 20:36:08 +0330

    resolve the conflict in index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 // import React from "react";
 // import ReactDOM from "react-dom";
 // // import './index.css';
 // import App from "./App";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
->>>>>>> aa5a41ab92933aefcc0ab7411a39f433a41f80f0
 
 // ReactDOM.render(
 //   <React.StrictMode>
